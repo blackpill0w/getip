d2b1272cae95e5d1120fc000bbb397fa36478126
blackpill0w <blackpill0w@protonmail.com>
2022-12-12 00:44:33 +0100

    Fixed program & Add `--port` option

--- main.c
+++ main.c
@@ -11,10 +11,11 @@
 
 void help() {
    printf(
-      "Usage: getip URL [-4|-6]\n"
-      "   -h, --help: Displays this message\n"
-      "   -4        : Display IPv4 only\n"
-      "   -6        : Display IPv6 only\n"
+      "Usage: getip URL [-4|-6] [-p/--port port]\n"
+      "   -h, --help: displays this help and exit\n"
+      "   -p, --port: specify port (80 by default, http)\n"
+      "   -4        : display IPv4 only\n"
+      "   -6        : display IPv6 only\n"
       );
 }
 
@@ -22,17 +23,21 @@ int main(int argc, char **argv) {
    bool only4 = false;
    bool only6 = false;
    char *url = NULL;
+   char *port = "80";
    if (argc < 2 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
       help();
-      exit(0);
+      exit(argc < 2 ? 1 : 0);
    }
 
+   bool get_port = false;
    for (int i = 1; i < argc; ++i) {
-      if (!strcmp(argv[i], "-4")) only4 = true;
+      if (get_port) port = argv[i];
+      else if (!strcmp(argv[i], "-4")) only4 = true;
       else if (!strcmp(argv[i], "-6")) only6 = true;
+      else if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "--port")) get_port = true, port = NULL;
       else url = argv[i];
    }
-   if (url == NULL) {
+   if (url == NULL || port == NULL) {
       help();
       exit(1);
    }
@@ -43,24 +48,35 @@ int main(int argc, char **argv) {
 
    struct addrinfo hints;
    memset(&hints, 0, sizeof(hints));
-   hints.ai_family = AF_UNSPEC;
+   hints.ai_family = only4 ? AF_INET : only6 ? AF_INET6 : AF_UNSPEC;
    hints.ai_socktype = SOCK_STREAM;
 
-   struct addrinfo *res;
-
-   if (getaddrinfo(url, "http", &hints, &res)) {
+   struct addrinfo *res = NULL;
+   if (getaddrinfo(url, port, &hints, &res)) {
+      freeaddrinfo(res);
       fprintf(stderr, "Invalid URL/IP or connection problem: %s\n", url);
       exit(1);
    };
 
    for (struct addrinfo *curr = res; curr != NULL; curr = curr->ai_next) {
-      if (only4 && curr->ai_family == AF_INET6) continue;
-      if (only6 && curr->ai_family == AF_INET) continue;
       char buf[INET6_ADDRSTRLEN] = {0};
-      inet_ntop(curr->ai_family, &curr, buf, curr->ai_addrlen);
+      void *addr;
+      if (curr->ai_family == AF_INET) {
+         struct sockaddr_in *s = (struct sockaddr_in *) curr->ai_addr;
+         addr = &(s->sin_addr);
+      }
+      else {
+         if (curr->ai_family == AF_INET6) {
+         struct sockaddr_in6 *s = (struct sockaddr_in6 *) curr->ai_addr;
+         addr = &(s->sin6_addr);
+      }
+      }
+
+      inet_ntop(curr->ai_family, addr, buf, sizeof(buf));
       printf("%s\n", buf);
    }
 
    freeaddrinfo(res);
+   res = NULL;
    return 0;
 }
